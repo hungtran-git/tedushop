3394fbbc76e5709f6086494098430bb89ca428ad
Trần Hà Hưng <hungtran9900@live.com>
2020-08-17 17:10:06 +0700

    commit lan 4

---
+++ 4.TeduShop.Model/Abtract/MetaInformation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TeduShop.Model.Abtract
+{
+    public class MetaInformation
+    {
+        [MaxLength(256)]
+        public string MetaKeyword { get; set; }
+
+        [MaxLength(256)]
+        public string MetaDescription { get; set; }
+    }
+}
